017df464b9c00431f428bbf4905e87957154722d
Rhoda Pickens <rhoda@Mac.lan>
2026-04-02 22:48:07 -0500

    changed hero text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -63,7 +63,7 @@ const Hero = () => {
                         Hi, my name is Rhoda Pickens
                     </h1>
                     <p className='text-center text-base tracking-wider text-slate-800 md:text-lg lg:text-xl'>I am a Full Stack Software Engineer 👩‍💻<br />
-                        - who loves coding and building visually pleasing experiences</p>
+                        - who loves coding and building beautiful websites</p>
                 </div>
                 <div className='mt-8 flex justify-center gap-x-10 text-2xl text-yellow-900 md:text-4xl'>
                     {heroIcons2.map((item, i) => (
